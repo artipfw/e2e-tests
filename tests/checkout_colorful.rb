53b00ff96a34363cf0792d3e873d4230a4e642d7
Andrey Kezikov <cheshirez@gmail.com>
2020-05-13 17:54:44 -0700

    Test fix for checkout

--- tests/checkout_colorful.rb
+++ tests/checkout_colorful.rb
@@ -2,13 +2,14 @@ base_url = ENV["URL"]
 
 # Visit the featured "Holy" sock from the front page
 visit("http://#{base_url}/")
-click_link "Holy"
+click_link "Colourful"
 
 # Assume that we don't have any socks yet
 assert! page.has_content?("0 items in cart")
 
 # Now add it to the cart
-click_link "Add to cart"
+page.find('#buttonCart').click
+
 assert! page.has_content?("1 item(s) in cart")
 
 # Start checkout
@@ -18,10 +19,15 @@ click_on("1 item(s) in cart")
 has_content?("1 item(s) in cart")
 
 # Check for correct Subtotal value on order page
-assert! page.find('th#orderSubtotal').text == '$99.99'
+assert! page.find('th#orderSubtotal').text == '$18.00'
 
-# TODO: to pass on final checkout page we need to add address/payment method or login
-# Be aware - system not flushing cart on failed runs
+# Address isn't works properly on the site, so using login
+first(:link, "Login").click
+fill_in "username", with: "Eve_Berger"
+fill_in "password", with: "eve"
+click_button("Log in")
 
-# assert! page.has_content?("My orders")
-# assert! page.has_content?("Your orders in one place.")
+click_on("Proceed to checkout")
+
+assert! page.has_content?("My orders")
+assert! page.has_content?("Your orders in one place.")
